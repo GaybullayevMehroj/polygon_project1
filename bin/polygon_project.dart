c72fdb8e66385f07d3d56a1e8af43994bf8ef212
GaybullayevMehroj <mehrojgaybullayev49@gmail.com>
2022-11-10 17:27:06 +0500

    polygon

--- bin/polygon_project.dart
+++ bin/polygon_project.dart
@@ -1,18 +1,23 @@
 // ignore_for_file: non_constant_identifier_names
 
 import 'package:polygon_project/circle.dart';
+import 'package:polygon_project/rectangle.dart';
+import 'package:polygon_project/Square.dart';
+import 'package:polygon_project/triangle.dart';
 
 void main() {
-  // Example of Circle
-  //ignore
-  Circle circle = Circle(4);
-  double circle_circumference = circle.circumference();
-  double circle_area = circle.area();
-
-  print("The circumference of the circle is:");
-  print(circle_circumference);
-// The circumference of the circle is: 25.132741228718345
-  print("The area of the circle is:");
-  print(circle_area);
-// The area of the circle is: 50.26548245743669
+  Circle x = Circle(5);
+  Rectangle y = Rectangle(4, 5);
+  Square z = Square(4);
+  Triangle t = Triangle(4, 5, 5);
+  print(x.pi);
+  print(x.r);
+  print(y.area());
+  print(y.perimeter());
+  print(z.area());
+  print(z.perimeter());
+  print(t.area());
+  print(t.perimeter());
+  print(t.isvalid());
+  print(t.getType());
 }
